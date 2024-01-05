a8c444ddd7d5692788e584d52a0bf320a73df3ef
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2024-01-05 16:31:37 -0300

    Criado autenticação por login e senha

--- api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
+++ api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
-public class AutentificacaoService implements UserDetailsService {
+public class AutenticacaoService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
     @Override
